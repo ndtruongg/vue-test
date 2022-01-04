02ad2ae9e11c1d8e40fa4f497711c8216e1ec8fa
truongnd-fabbi <truongnd2@fabbi.io>
2022-01-04 16:06:31 +0700

    update chat

--- src/components/organisms/Chat.vue
+++ src/components/organisms/Chat.vue
@@ -1,45 +1,55 @@
 <template>
   <div class="chat">
     <div class="chat-room">
-      <h3 v-if="auth">Welcome back {{ auth.displayName }}</h3>
+      <h3 v-if="auth">Chats</h3>
       <button v-else class="btn" @click="loginWithGoogle">
         Login with google
       </button>
 
       <button
-        v-for="item of rooms"
-        :key="item.id"
-        :class="[item.id === roomActive ? 'active' : '', 'r']"
-        @click="chooseRoom(item.id)"
+        v-for="(item, key) of users"
+        :key="key"
+        :class="[checkActiveMsg(item.id) ? 'active' : '', 'list-user__item']"
+        @click="openChatHistories(item.email)"
       >
-        {{ item.id }}
+        <div class="_avatar">
+          <img :src="item.avatar" :alt="item.nickname" />
+          <span :class="[item.online === true ? 'active' : '', 'dot']"></span>
+        </div>
+        {{ item.nickname }}
       </button>
     </div>
-    <div class="chat-detail">
-      <div class="chat-history" id="chat-histories">
-        <div
-          v-for="item of histories"
-          :key="item.id"
-          :class="[item.userId === auth.uid ? 'rtl' : 'ltr', 'mes-box']"
-        >
-          <div class="mes">{{ item.content }}</div>
+    <div v-if="auth" class="chat-detail">
+      <template v-if="targetInfo">
+        <ChatDetailHeader :userData="targetInfo" />
+      </template>
+      <div class="chat-detail__body">
+        <div class="chat-history" id="chat-histories">
+          <div
+            v-for="(item, key) of messages"
+            :key="key"
+            :class="[item.userId.includes(auth.uid) ? 'rtl' : 'ltr', 'mes-box']"
+          >
+            <div class="mes">{{ item.content }}</div>
+            <span class="time">{{ getTime(item.createdAt) }}</span>
+          </div>
         </div>
+        <form v-if="targetInfo" class="chat-form" @submit.prevent="sentMessage">
+          <input
+            type="text"
+            v-model="messageInput"
+            placeholder="Send a message"
+          />
+          <button
+            :class="{ active: messageInput }"
+            :disabled="messageInput.length === 0"
+          >
+            <span class="material-icons-round">
+              send
+            </span>
+          </button>
+        </form>
       </div>
-      <form class="chat-form" @submit.prevent="sentMessage">
-        <input
-          type="text"
-          v-model="messageInput"
-          placeholder="Send a message"
-        />
-        <button
-          :class="{ active: messageInput }"
-          :disabled="messageInput.length === 0"
-        >
-          <span class="material-icons-round">
-            send
-          </span>
-        </button>
-      </form>
     </div>
   </div>
 </template>
@@ -53,39 +63,45 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-// import dayjs from '@/plugins/dayjs';
 import {
   collection,
   query,
-  // getDocs,
   addDoc,
   orderBy,
   onSnapshot,
   setDoc,
   doc,
+  updateDoc,
+  where,
+  getDocs,
 } from "firebase/firestore";
+
+import ChatDetailHeader from "./ChatDetailHeader.vue";
 
 const provider = new GoogleAuthProvider();
 export default {
   name: "Chat",
+  components: { ChatDetailHeader },
   data() {
     return {
       messageInput: "",
-      histories: [],
+      histories: {
+        uid: "",
+        messages: [],
+      },
       auth: null,
       token: "",
       rooms: [],
       roomActive: "",
+      userFB: null,
+      users: [],
+      targetInfo: null,
     };
   },
 
   async created() {
     // get auth info
-    this.getAuthStatus();
-  },
-
-  updated() {
-    this.scrollToBottom();
+    await this.getAuthStatus();
   },
 
   mounted() {
@@ -102,6 +118,7 @@ export default {
       });
     });
 
+    // get rooms
     const queryRoom = query(
       collection(db, "room"),
       orderBy("createdAt", "asc")
@@ -114,6 +131,56 @@ export default {
         }
       });
     });
+
+    // get users
+    let emailSignIn = localStorage.getItem("emailForSignIn");
+    const queryUser = query(
+      collection(db, "user"),
+      where("email", "!=", emailSignIn)
+    );
+    onSnapshot(queryUser, (querySnapshot) => {
+      querySnapshot.forEach((docRef) => {
+        let checkExistDocument = this.users.find(
+          (item) => item.id === docRef.id
+        );
+        if (!checkExistDocument) {
+          let emailAuth = localStorage.getItem("emailForSignIn");
+          if (emailAuth != docRef.data().email) {
+            this.users.push({
+              ...docRef.data(),
+              id: docRef.id,
+            });
+          }
+        }
+      });
+    });
+
+    // before browser or tab close
+    window.addEventListener("beforeunload", async function() {
+      let authToken = localStorage.getItem("firebase-auth-token");
+
+      await updateDoc(doc(db, "user", authToken), {
+        online: false,
+      });
+    });
+  },
+
+  updated() {
+    this.scrollToBottom();
+  },
+
+  async beforeDestroy() {
+    let authToken = localStorage.getItem("firebase-auth-token");
+
+    await updateDoc(doc(db, "user", authToken), {
+      online: false,
+    });
+  },
+
+  computed: {
+    messages() {
+      return this.histories?.messages;
+    },
   },
 
   methods: {
@@ -121,12 +188,21 @@ export default {
       let msg = this.messageInput;
       this.messageInput = "";
       if (msg.length > 0) {
-        await addDoc(collection(db, "chat"), {
-          name: this.auth?.displayName,
+        if (!this.histories.uid) {
+          const docRef = await addDoc(collection(db, "room"), {
+            createdAt: new Date(),
+            userIds: [this.auth.uid, this.targetInfo.id],
+          });
+          this.histories.uid = docRef.id;
+        }
+
+        await addDoc(collection(db, "room", this.histories.uid, "messages"), {
           content: msg,
-          timestamp: new Date(),
-          userId: this.auth?.uid,
+          createdAt: new Date(),
+          userId: "user/" + this.auth?.uid,
         });
+
+        this.getMessages();
       }
     },
 
@@ -139,11 +215,15 @@ export default {
       signInWithPopup(getAuth(), provider)
         .then(async (result) => {
           this.auth = result.user;
-
           await setDoc(doc(db, "user", this.auth.uid), {
             nickname: this.auth?.displayName,
             avatar: this.auth?.photoURL,
+            email: this.auth?.email,
+            online: true,
           });
+
+          localStorage.setItem("firebase-auth-token", this.auth.uid);
+          localStorage.setItem("emailForSignIn", this.auth.email);
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -160,15 +240,100 @@ export default {
     },
 
     async getAuthStatus() {
-      await onAuthStateChanged(getAuth(), (user) => {
+      await onAuthStateChanged(getAuth(), async (user) => {
         if (user) {
           this.auth = user;
+          let authToken = localStorage.getItem("firebase-auth-token");
+
+          await updateDoc(doc(db, "user", authToken), {
+            online: true,
+          });
         }
       });
     },
 
     chooseRoom(roomId) {
       this.roomActive = roomId;
+    },
+
+    async openChatHistories(email) {
+      const q = query(collection(db, "user"), where("email", "==", email));
+      this.histories = { uid: "", messages: [] };
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((docRef) => {
+        this.targetInfo = { id: docRef.id, ...docRef.data() };
+      });
+
+      if (this.targetInfo) {
+        const qHistories = query(
+          collection(db, "room"),
+          where("userIds", "in", [
+            [this.auth?.uid, this.targetInfo.id],
+            [this.targetInfo.id, this.auth.uid],
+          ])
+        );
+
+        const historySnapshot = await getDocs(qHistories);
+        historySnapshot.forEach((docRef) => {
+          this.histories.uid = docRef.id;
+        });
+      }
+
+      if (this.histories.uid) {
+        this.getMessages();
+      }
+    },
+
+    async getMessages() {
+      const qMessages = query(
+        collection(db, "room", this.histories.uid, "messages"),
+        orderBy("createdAt", "asc")
+      );
+
+      await onSnapshot(qMessages, (messageSnapshot) => {
+        messageSnapshot.forEach((docRef) => {
+          let checkExistDocument = this.histories.messages.find(
+            (item) => item.id === docRef.id
+          );
+          if (!checkExistDocument) {
+            this.histories.messages.push({ id: docRef.id, ...docRef.data() });
+          }
+        });
+      });
+    },
+
+    async findRoom() {
+      if (this.targetInfo) {
+        const qHistories = query(
+          collection(db, "room"),
+          where("userIds", "in", [
+            [this.auth?.uid, this.targetInfo.id],
+            [this.targetInfo.id, this.auth.uid],
+          ])
+        );
+
+        const historySnapshot = await getDocs(qHistories);
+        historySnapshot.forEach((docRef) => {
+          this.histories.uid = docRef.id;
+        });
+      }
+    },
+
+    checkActiveMsg(uid) {
+      if (this.targetInfo) {
+        if (this.targetInfo.id === uid) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+
+    getTime(createdAt) {
+      let time = new Date(
+        createdAt.seconds * 1000 + createdAt.nanoseconds / 1e6
+      );
+      return this.$dayjs(time).format("HH:mm");
     },
   },
 };
@@ -178,14 +343,15 @@ export default {
 .chat {
   border: 1px solid #cdcdcc;
   border-radius: 10px;
-  padding: 10px;
   display: flex;
 
   &-room {
+    padding: 12px;
+    padding-right: 20px;
     display: flex;
     flex-direction: column;
-    width: calc(100% - 350px);
-    padding-right: 20px;
+    width: 300px;
+    border-right: 1px solid #cdcdcd;
     .r {
       background: #cdcdcc;
       text-align: left;
@@ -199,9 +365,16 @@ export default {
     }
   }
 
+  &-detail {
+    width: calc(100% - 300px);
+
+    &__body {
+      padding: 12px;
+    }
+  }
+
   &-history {
-    width: 350px;
-    height: 350px;
+    height: 650px;
     overflow: auto;
     padding-right: 3px;
     margin-bottom: 10px;
@@ -224,10 +397,23 @@ export default {
 
     .mes-box {
       display: flex;
+      align-items: flex-start;
+      .time {
+        font-size: 12px;
+        margin-top: 12px;
+        margin-left: 12px;
+        color: #888;
+      }
       &.rtl {
         justify-content: flex-end;
 
+        .time {
+          order: 1;
+          margin-right: 12px;
+        }
+
         .mes {
+          order: 2;
           background-color: #2980b9;
           border-radius: 20px 18px 5px 20px;
         }
@@ -278,6 +464,70 @@ export default {
         }
       }
     }
+  }
+
+  .list-user {
+    &__item {
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      margin-bottom: 10px;
+      &:hover,
+      &.active {
+        background: #f1f1f1;
+        border-radius: 10px;
+      }
+
+      ._avatar {
+        width: 40px;
+        height: 40px;
+        position: relative;
+        margin-right: 10px;
+
+        .dot {
+          position: absolute;
+          z-index: 2;
+          bottom: 0px;
+          right: 0px;
+        }
+
+        img {
+          width: 40px;
+          border-radius: 50%;
+          height: 40px;
+        }
+      }
+
+      .dot {
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        background: #cecece;
+
+        &.active {
+          background: #18eb05;
+        }
+      }
+    }
+  }
+}
+
+.insert {
+  &-enter,
+  &-leave-to {
+    transform: translateY(10px);
+    opacity: 0;
+  }
+
+  &-enter-active,
+  &-leave-active {
+    transition: ease 0.4s all;
+  }
+
+  &-enter-to,
+  &-leave {
+    opacity: 1;
+    transform: translateY(0);
   }
 }
 </style>
---
+++ src/components/organisms/ChatDetailHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <div class="avatar">
+      <img :src="userData.avatar" :alt="userData.nickname" />
+    </div>
+
+    <h3>{{ userData.nickname }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatDetailHeader",
+  props: {
+    userData: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #cecece;
+  .avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 10px;
+    overflow: hidden;
+  }
+
+  h3 {
+    font-size: 18px;
+    margin-bottom: 0;
+  }
+}
+</style>
